3a77ae4a5b3f5400e8a6d1a08c19c15e12c2500b
Phetolo Tshukudu <phetolotshukudu@gmail.com>
2026-02-20 10:24:35 +0200

    Removed unneccesary zip folder

--- src/Model/Transaction.java
+++ src/Model/Transaction.java
@@ -4,13 +4,14 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Transaction {
-	private int id;
-	private double amount;
-	private String description;
-	private Category category;
-	private LocalDate date;
+	private  int id;
+	private final double amount;
+	private final String description;
+	private final Category category;
+	private final LocalDate date;
 	
 	public Transaction(double amount, String description, Category category, LocalDate date) {
+		this.id = 0;
 		this.amount = amount;
 		this.description = description;
 		this.category = category;
@@ -45,33 +46,21 @@ public class Transaction {
 		return amount;
 	}
 
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
 
 	public String getDescription() {
 		return description;
 	}
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
+	
 	public Category getCategory() {
 		return category;
 	}
 
-	public void setCategory(Category category) {
-		this.category = category;
-	}
-
+	
 	public LocalDate getDate() {
 		return date;
 	}
 
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
 	
 	public boolean isIncome() {
 		if(amount>0) 
